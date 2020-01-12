2aab9d22b4c39e6c6bda51ddf7e11e237c425ff1
sabadom1 <markdavid.sabado@mail.utoronto.ca>
2020-01-12 09:28:22 -0500

    location_list function

--- martello/pgame.py
+++ martello/pgame.py
@@ -25,6 +25,13 @@ def load_data(filename: str) -> dict:
         d = json.load(f)
     return d
 
+def location_list(people: list, dataset: dict) -> list:
+    mega_list = []
+    for person in people:
+        mega_list.append(create_event_by_person(person, dataset))
+    print(mega_list)
+    return mega_list
+
 def mssg(m, ft, x, y):
     TextSurf, TextRect = text_objects(m, ft)
     TextRect.center = (x, y)
@@ -50,16 +57,9 @@ def button(msg, x, y, w, h, ic, ac, action=None):
                 elif action == 'cs':
                     char_selection()
                 elif action == 'sel':
-<<<<<<< HEAD
                     print("hello")
                     pygame.draw.rect(win, ac, (x, y, w, h))
                     # button('msg', x, y, w, h, (255, 255, 0), (255, 255, 0), 'sel')
-=======
-                    selected_names.append(msg) if msg not in selected_names else selected_names.remove(msg)
-                    print(selected_names)
-                    # pygame.draw.rect(win, ac, (x, y, w, h))
-                    # pygame.display.update()
->>>>>>> 265275104c9f0352684837e4978924152ac492de
     else:
         pygame.draw.rect(win, ic, (x, y, w, h))
     mssg(msg, pygame.font.Font("freesansbold.ttf", 20), x+(w/2), y+(h/2))
@@ -122,8 +122,11 @@ def core():
     run = True
 
     p_list = create_event_by_person("Jason", dataset)
-    # print(p_list)
     p_position = 0
+
+    # act = action(win,)
+
+    location_list(["Eugene"], dataset)
 
     while run:
         win.fill((0, 0, 0))
