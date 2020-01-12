6eba7f6575f3aed4deb51ecc8519f2c460ea2334
sabadom1 <markdavid.sabado@mail.utoronto.ca>
2020-01-12 03:20:01 -0500

    pgame?

--- martello/pgame.py
+++ martello/pgame.py
@@ -1,85 +1,91 @@
 import pygame
+import json
+# import action
+import csv
+
+def create_location(d):
+    location
 
 pygame.init()
 
-win = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Murder on the 2nd Floor")
+with open("dataset.json", 'r') as f:
+    d = json.load(f)
+
+eugene_list = []
+eugene_position = 0
+
+with open("location_data.json", 'r') as f2:
+    location_data = json.load(f2)
+   
+
+for i in d.keys():
+    if d[i]["guest-id"] == "Eugene": 
+        #add to list
+        eugene_list.append(d[i])
+
+# print(eugene_list)
+
+win = pygame.display.set_mode((1080,600))
+pygame.display.set_caption("First Game")
 fr = pygame.image.load("floor_rotated2.png")
-clock = pygame.time.Clock()
+fr = pygame.transform.scale(fr, (1080, 600))
 
-def text_objects(text, font):
-    ts = font.render(text, True, (0, 0, 0))
-    return ts, ts.get_rect()
+x = 50
+y = 50
+width = 20
+height = 20
+val = 5
 
-def button(msg,x,y,w,h,ic,ac):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
+run = True
 
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(win, ac,(x,y,w,h))
-        if click[0] == 1:
-            core()
-    else:
-        pygame.draw.rect(win, ic,(x,y,w,h))
+# list = {}
 
-    smallText = pygame.font.Font("freesansbold.ttf",20)
-    textSurf, textRect = text_objects(msg, smallText)
-    textRect.center = ( (x+(w/2)), (y+(h/2)) )
-    win.blit(textSurf, textRect)
+while run:
+    # if (eugene_position > eugene_list.len()):
+    #     print("finished")
+    
+    win.fill((0,0,0))
+    win.blit(fr, (0,0))
+    pygame.time.delay(100)
 
-def intro():
-    intro = True
-    while intro:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-        win.fill((255, 255, 255))
-        lt = pygame.font.Font('FreeSansBold.ttf', 55)
-        TextSurf, TextRect = text_objects("Murder on the 2nd Floor", lt)
-        TextRect.center = (400, 300)
-        win.blit(TextSurf, TextRect)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-        button('Start',150,450,100,50,(0,200,0),(0,255,0))
-        button('Quit',550,450,100,50,(200,0,0),(255,0,0))
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        x -= val
 
-        pygame.display.update()
-        clock.tick(15)
+    if keys[pygame.K_RIGHT]:
+        x += val
 
-def core():
-    x = 50
-    y = 50
-    width = 40
-    height = 60
-    vel = 5
-    run = True
-    while run:
-        win.fill((0,0,0))
-        win.blit(fr, (0,0))
-        pygame.time.delay(100)
+    if keys[pygame.K_UP]:
+        y -= val
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+    if keys[pygame.K_DOWN]:
+        y += val
 
-        keys = pygame.key.get_pressed()
+    for location in location_data:
+        if (location["device-id"] == eugene_list[eugene_position]["device-id"]):
+            print(location["device-id"])
+            if (x < location["x"]):
+                x += val
+            elif (x > location["x"]):
+                x -= val
+            elif (y < location["y"]):
+                y += val 
+            elif (y > location["y"]):
+                y -= val
+            else:
+                eugene_position += 1
+        # print(location["device-id"])
+        # print(eugene_list[eugene_position]["device-id"])
+        # print("ff")
 
-        if keys[pygame.K_LEFT]:
-            x -= vel
-
-        if keys[pygame.K_RIGHT]:
-            x += vel
-
-        if keys[pygame.K_UP]:
-            y -= vel
-
-        if keys[pygame.K_DOWN]:
-            y += vel
-
-        pygame.draw.rect(win, (255,0,0), (x, y, width, height))
-        pygame.display.update()
-        clock.tick(60)
-
-
-intro()
-core()
+    
+    
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
+    pygame.display.update() 
+    
 pygame.quit()
